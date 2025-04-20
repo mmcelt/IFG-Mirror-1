4a1cb5055b4a4bd81512436eef9aed25a845a097
mmcelt <mmcelt@yahoo.com>
2025-04-19 18:23:59 -0600

    Working on StickerManager. Made range stickers & positions

--- Assets/Scripts/Managers/StickerManager.cs
+++ Assets/Scripts/Managers/StickerManager.cs
@@ -2,9 +2,14 @@ using Mirror;
 using UnityEngine;
 
 public class StickerManager : SingletonMirror<StickerManager>
-{	
+{
    #region Fields & Properties
 
+   [SerializeField] GameObject[] commodityStickers;
+   [SerializeField] GameObject[] equipmentStickers;
+   [SerializeField] GameObject[] rangeStickers;
+
+   GameObject stickerToPlace;
 
    #endregion
 
@@ -13,5 +18,19 @@ public class StickerManager : SingletonMirror<StickerManager>
  
    #endregion
 
+   public void PlaceBoardSticker(string farmer, string type, int amount, bool isdoubled = false)
+   {
+      //GetStickerToPlace(type);
+      //GetSpawnPosition(farmer, type);
+   }
+
+   //GameObject GetStickerToPlace(string type)
+   //{
+   //   switch (type)
+   //   {
+   //      case IFG.HAY:
+
+   //   }
+   //}
 }
 
